e62afa56ab1e6f43b2f91f3321449cf992a1199e
yushinc <cys74351@gmail.com>
2024-01-14 23:07:28 +0900

    feat: project setting

---
+++ src/main/java/jpa/bookExample/BookExampleApplication.java
@@ -0,0 +1,13 @@
+package jpa.bookExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa/bookExample/BookExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.bookExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
